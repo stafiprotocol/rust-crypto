bed22e9374f73a60029c415bdc1cf80638a774d2
Howie <howie@bitstarlight.com>
2019-11-23 16:37:38 +0800

    Fix core

--- src/cryptoutil.rs
+++ src/cryptoutil.rs
@@ -8,6 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#[cfg(not(feature = "std"))]
 use core::result;
 use sr_std;
 use sr_std::marker::*;
